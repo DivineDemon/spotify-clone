40bdaac2cf949669e1e973808e71398e98e77f86
DivineDemon <supame123@gmail.com>
2022-05-05 22:10:35 +0400

    Completed! :tada: ✨

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { getSession } from "next-auth/react";
 import Sidebar from "../components/Sidebar";
 import Center from "../components/Center";
+import Player from "../components/Player";
 
 export default function Home() {
   return (
@@ -16,7 +17,9 @@ export default function Home() {
         <Center />
       </main>
 
-      <div>{/* Player */}</div>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   );
 }
